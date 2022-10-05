a463b465e686fc8d0c9fab461620cf0dbe4d6b01
Yashvi Soni <113103078+yashzinz@users.noreply.github.com>
2022-10-05 10:33:43 +0400

    Add files via upload

---
+++ Chapter 1- Getting Started with Python/Exercises/Excersice 1.py
@@ -0,0 +1,14 @@
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star, \n\tHow I wonder what you are")
+import sys
+print(sys.version)
+import datetime
+now=datetime.datetime.now()
+print(now)
+x="My name "
+y="is "
+z="Yashvi"
+a=x+y+z 
+print(a)
+radius=int(input("Enter radius of a circle"))
+area=3.14*radius**2
+print("AREA OF THE CIRCLE=", area)
