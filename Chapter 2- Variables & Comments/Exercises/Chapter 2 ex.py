2ea37ddf40e7f16d6ae8fcb9c7710015ac54524d
Yashvi Soni <113103078+yashzinz@users.noreply.github.com>
2022-10-25 09:36:00 +0400

    chap 2&3

---
+++ Chapter 2- Variables & Comments/Exercises/Chapter 2 ex.py
@@ -0,0 +1,22 @@
+#Ex 1
+x="I like reading books"
+print(x)
+x="I like watching anime"
+print(x)
+#Ex 2
+print("“Sometimes it is the people no one can imagine anything of who do the things no one can imagine.\n\t\t\t\t-Alan Turing”")
+#Ex 3
+y="\tYashvi \n Zinzuadia"
+print(y)
+print(y.lstrip())
+print(y.rstrip())
+print(y.strip())
+#Ex 4
+x=6
+x="My favorite number is=" ,x
+print(x)
+#Ex 5
+Total_amount=50
+Cost_of_each_USB_stick=6
+print("Number of usb sticks the girl can buy=",Total_amount//Cost_of_each_USB_stick)
+print("Amount left with her=",Total_amount%Cost_of_each_USB_stick)
---
+++ Chapter 3- Data Structures/Exercises/Chap 3 ex.py
@@ -0,0 +1,12 @@
+#Ex 1
+friends=["Anantha","Sneha","Rithisha","Emaan"]
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+#EX 2 
+print("I'm Yashvi's friend,", friends[0])
+print("I'm Yashvi's friend,", friends[1])
+print("I'm Yashvi's friend,", friends[2])
+print("I'm Yashvi's friend,", friends[3])
+#Ex 3
